40429053c9be40983197d4cf0099c9e006014f31
Felix Medina <felix554@gmail.com>
2022-12-10 08:58:41 -0400

    CORS y EXpress

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-const express = require('express'); //Linea de el como se importan las librerias en NODE
 require('dotenv').config();
+
+const express = require('express'); //Linea de el como se importan las librerias en NODE
+const cors = require('cors'); //Permite peticiones desde varios Dominios si asi lo deseas
+
 const { dbConnection } = require('./database/config');//Va entre llaves para desestructurar si contiene otros valores el objeto y mas adelante le quiera agregar otros valores
 
 //Crear el servidor de Expres
 const app = express(); //Crea o inicializa la aplicacion de Express
+
+//Configurar CORS
+app.use(cors());
+/**
+ * use es como un midelwer que ejecuta la funcion cors a todas las lineas que estan hacia abajo
+ */
 
 //Base de Datos
 dbConnection();
